2a2aeb0d8548fbdcb3867e3ef80f4834c98cf0f7
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-04 19:00:31 -0500

    Grab DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 // import functions and grab DOM elements
+const statsSectionEl = document.querySelector('#stats-section');
+const goblinsVanquishedDivEl = document.querySelector('#goblins-vanquished-div');
+const goblinsVanquishedEl = document.querySelector('#goblins-vanquished');
+
+const playerSectionEl = document.querySelector('#player-section');
+const playerAvatarDivEl = document.querySelector('#player-avatar-div');
+const playerAvatarEl = document.querySelector('#player-section');
+const playerStatsEl = document.querySelector('#player-stats');
+
+const matchupSectionEl = document.querySelector('#matchup-section');
+const matchupMessageEl = document.querySelector('#matchup-message');
+
+const goblinsSectionEl = document.querySelector('#goblins-section');
+const addGoblinFormEl = document.querySelector('#add-goblin-form');
+const goblinNameInputEl = document.querySelector('#goblin-name-input');
+const addGoblinButton = document.querySelector('#add-goblin-button');
 
 // let state
 
